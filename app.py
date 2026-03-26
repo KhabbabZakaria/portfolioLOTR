31d06d10f3acdc6bb754cc71b7e881ec6a1bcc79
Khabbab Zakaria <46716277+KhabbabZakaria@users.noreply.github.com>
2026-03-26 11:36:55 +0100

    new website

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/classic')
+def classic():
+    return render_template('lotr_index.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
